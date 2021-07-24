24f30673841ff51199ffcaa8134901a6c62aa6f4
jack.brightsoft <jack.brightsoft@gmail.com>
2021-07-24 20:45:59 +0700

    feat(product): add tags to product

--- src/containers/Hiddens/AddProduct.js
+++ src/containers/Hiddens/AddProduct.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { Row, Col } from "antd";
-import { addProduct } from "../../services/firebaseService";
-import "./AddProduct.scss";
-import Dashboard from "../Dashboard/Dashboard";
-import "./AddProduct.scss";
+import React, { useState } from "react"
+import { Row, Col } from "antd"
+import { addProduct, addTags } from "../../services/firebaseService"
+import "./AddProduct.scss"
+import Dashboard from "../Dashboard/Dashboard"
+import "./AddProduct.scss"
 
 const products = [
 	{
@@ -13,6 +13,7 @@ const products = [
 		img: "https://firebasestorage.googleapis.com/v0/b/test-df518.appspot.com/o/1_ef8f0db1-9878-4d5c-9421-984054e785a5_600x.jpg?alt=media&token=8554785c-6280-4c7f-bc08-a33ed3c8a3d2",
 		description:
 			"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque New producteu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.",
+		tags: "tag1,tag2"
 	},
 	{
 		name: "Pellentesque lacinia",
@@ -21,6 +22,7 @@ const products = [
 		img: "https://firebasestorage.googleapis.com/v0/b/test-df518.appspot.com/o/2_bede556b-ba58-485a-a85d-8756d110b5df_900x900.jpg?alt=media&token=8832722e-aa0f-49f9-a3bb-ac57f3eb5f50",
 		description:
 			"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque New producteu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.",
+		tags: "tag1,tag3"
 	},
 	{
 		name: "Orara Valiley Honey",
@@ -29,6 +31,7 @@ const products = [
 		img: "https://firebasestorage.googleapis.com/v0/b/test-df518.appspot.com/o/3_ec7e2813-6fbc-40ce-a776-c9da15ec9022_425x.jpg?alt=media&token=f77d0275-06ab-4aac-9943-2a20bba349a3",
 		description:
 			"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque New producteu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.",
+		tags: "tag3,tag4"
 	},
 	{
 		name: "Aenean commodo",
@@ -37,6 +40,7 @@ const products = [
 		img: "https://firebasestorage.googleapis.com/v0/b/test-df518.appspot.com/o/4_507f6fba-f388-4083-9fb4-e2da9dfda4ee_425x.webp?alt=media&token=42f6bf39-ddb3-4f16-b5dd-ba093eded36e",
 		description:
 			"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque New producteu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.",
+		tags: "tag2,tag3"
 	},
 	{
 		name: "Nulla consequat ma",
@@ -45,6 +49,7 @@ const products = [
 		img: "https://firebasestorage.googleapis.com/v0/b/test-df518.appspot.com/o/11_600x.jpg?alt=media&token=6601a271-9146-490e-abd0-6f2e58eeace4",
 		description:
 			"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque New producteu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.",
+		tags: "tag4,tag5"
 	},
 	{
 		name: "Morbi viverra hend",
@@ -53,6 +58,7 @@ const products = [
 		img: "https://firebasestorage.googleapis.com/v0/b/test-df518.appspot.com/o/12_600x.jpg?alt=media&token=64acd3e6-b253-45bc-8924-5aa355717589",
 		description:
 			"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque New producteu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.",
+		tags: "tag3,tag6"
 	},
 	{
 		name: "Magnis dis parturient",
@@ -61,6 +67,7 @@ const products = [
 		img: "https://firebasestorage.googleapis.com/v0/b/test-df518.appspot.com/o/5_600x.jpg?alt=media&token=96cf3483-c2b6-4b0c-a6e4-02399e977abd",
 		description:
 			"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque New producteu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.",
+		tags: "tag3,tag6"
 	},
 	{
 		name: "Donec quam felis",
@@ -69,6 +76,7 @@ const products = [
 		img: "https://firebasestorage.googleapis.com/v0/b/test-df518.appspot.com/o/6_c18dfaa8-37e0-44ad-bc5b-31f0c5b06c41_600x.jpg?alt=media&token=ab26e714-d826-4f39-9eaa-dc687cc6397d",
 		description:
 			"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque New producteu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.",
+		tags: "tag3,tag6"
 	},
 	{
 		name: "Cum sociis natoque",
@@ -77,6 +85,7 @@ const products = [
 		img: "https://firebasestorage.googleapis.com/v0/b/test-df518.appspot.com/o/7_f96edd11-989e-46e3-9f2e-c0bc8480df7e_600x.jpg?alt=media&token=9a299733-b6f0-489f-974d-475e878f3fdf",
 		description:
 			"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque New producteu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.",
+		tags: "tag3,tag6"
 	},
 	{
 		name: "Cras fringilla et",
@@ -85,6 +94,7 @@ const products = [
 		img: "https://firebasestorage.googleapis.com/v0/b/test-df518.appspot.com/o/8_669ffe79-f0f9-451b-a870-684415525cb5_600x.jpg?alt=media&token=eb79447b-d463-4fcc-9ccf-2a85af174df6",
 		description:
 			"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque New producteu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.",
+		tags: "tag3,tag6"
 	},
 	{
 		name: "Consectetuer adipiscing",
@@ -93,8 +103,9 @@ const products = [
 		img: "https://firebasestorage.googleapis.com/v0/b/test-df518.appspot.com/o/9_e1d315f2-9c92-4d8e-92d5-817b7bebbc2e_600x.jpg?alt=media&token=53c6dca2-3e47-477d-bb47-8df2520913a2",
 		description:
 			"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque New producteu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.",
+		tags: "tag3,tag6"
 	},
-];
+]
 
 const initData = {
 	name: "",
@@ -102,36 +113,44 @@ const initData = {
 	sale: 0,
 	img: "",
 	description: "",
-};
+	tags: ""
+}
 
 const AddProduct = () => {
-	const [data, setData] = useState({ ...initData });
+	const [data, setData] = useState({ ...initData })
 
 	const changeData = (valueObj) => {
 		setData({
 			...data,
 			...valueObj,
-		});
-	};
+		})
+	}
 
 	const add = async (product) => {
 		try {
-			const { name, price, img, description } = product;
+			const { name, price, img, description, tags } = product
 			if ([name, price, img, description].some((value) => !value))
-				throw new Error("Invalid data");
-			await addProduct(product);
-			alert("Add product successfully");
+				throw new Error("Invalid data")
+			if (tags && tags.trim()) {
+				const productTags = tags.split(",").map(tag => tag.trim())
+				await addTags(productTags)
+			}
+			await addProduct({
+				...product,
+				tags: `,${tags},`
+			})
+			alert("Add product successfully")
 		} catch (err) {
-			console.error(err.response?.data || err.message);
+			console.error(err.response?.data || err.message)
 		}
-	};
+	}
 
 	const addSampleProducts = async () => {
 		for (const product of products) {
-			await add(product);
+			await add(product)
 		}
-		alert("Added sample products");
-	};
+		alert("Added sample products")
+	}
 
 	return (
 		<>
@@ -181,6 +200,20 @@ const AddProduct = () => {
 									placeholder="Sale"
 								/>
 							</div>
+
+							<div class="form-group">
+								<label for="price" class="form-label">
+									Tags
+								</label>
+								<input
+									className="form-input"
+									value={data.tags}
+									onChange={(e) =>
+										changeData({ tags: e.target.value })
+									}
+									placeholder="Tags, split by commas"
+								/>
+							</div>
 						</Col>
 						<Col span={11}>
 							<div class="form-group">
@@ -216,7 +249,7 @@ const AddProduct = () => {
 				</div>
 			</Dashboard>
 		</>
-	);
-};
+	)
+}
 
-export default AddProduct;
+export default AddProduct
--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -1,150 +1,165 @@
-import firebase from "../configs/firebase";
+import firebase from "../configs/firebase"
 
 const collections = {
 	products: "products",
 	carts: "carts",
-};
+	tags: "tags"
+}
 
 // Database features
 export const addProduct = async (data) => {
-	await firebase.firestore().collection(collections.products).add(data);
-};
+	await firebase.firestore().collection(collections.products).add(data)
+}
+
+export const addTags = async (tags) => {
+	const query = await firebase
+		.firestore()
+		.collection(collections.tags)
+		.get()
+
+	const allTags = query.docs.map(doc => doc.data().name)
+
+	for (const tag of tags) {
+		if (allTags.includes(tag)) continue
+		await firebase.firestore().collection(collections.tags).add({ name: tag })
+	}
+}
 
 export const removeProduct = async (productName) => {
-  let query = firebase.firestore().collection(collections.products);
-	query = query.where("name", "==", productName);
+	let query = firebase.firestore().collection(collections.products)
+	query = query.where("name", "==", productName)
 
-	const snapshot = await query.get();
-	snapshot.forEach((doc) => doc.ref.delete());
+	const snapshot = await query.get()
+	snapshot.forEach((doc) => doc.ref.delete())
 
-	alert("Product is removed");
-};
+	alert("Product is removed")
+}
 
 export const countProducts = async () => {
 	const data = await firebase
 		.firestore()
 		.collection(collections.products)
-		.get();
+		.get()
 
-	return data.size;
-};
+	return data.size
+}
 
 export const getProducts = async (offset, sortBy, sortOrder, limit) => {
 	let query = firebase
 		.firestore()
 		.collection(collections.products)
-		.orderBy(sortBy, sortOrder);
+		.orderBy(sortBy, sortOrder)
 
-	const data = await query.get();
+	const data = await query.get()
 
 	return data.docs.slice(offset, offset + limit).map((doc) => ({
 		id: doc.id,
 		...doc.data(),
-	}));
-};
+	}))
+}
 
 export const getProductById = async (id) => {
 	const doc = await firebase
 		.firestore()
 		.collection(collections.products)
 		.doc(id)
-		.get();
+		.get()
 
-	return doc.data();
-};
+	return doc.data()
+}
 
 // Cart features
 export const addToCart = async (productId, number, isAll = false) => {
-	const userId = firebase.auth().currentUser?.uid;
-	if (!userId) return;
+	const userId = firebase.auth().currentUser?.uid
+	if (!userId) return
 
-	let query = firebase.firestore().collection(collections.carts);
-	query = query.where("userId", "==", userId);
-	query = query.where("productId", "==", productId);
-	const data = await query.get();
+	let query = firebase.firestore().collection(collections.carts)
+	query = query.where("userId", "==", userId)
+	query = query.where("productId", "==", productId)
+	const data = await query.get()
 
-	const cart = data.docs;
+	const cart = data.docs
 	if (!!cart && !!cart.length) {
-		const item = cart[0];
+		const item = cart[0]
 
 		await firebase
 			.firestore()
 			.collection(collections.carts)
 			.doc(item.id)
-			.update({ count: isAll ? number : item.data().count + number });
+			.update({ count: isAll ? number : item.data().count + number })
 
-		return;
+		return
 	}
 
 	await firebase
 		.firestore()
 		.collection(collections.carts)
-		.add({ userId, productId, count: number });
-};
+		.add({ userId, productId, count: number })
+}
 
 export const getCart = async () => {
-	const userId = firebase.auth().currentUser?.uid;
-	if (!userId) return [];
+	const userId = firebase.auth().currentUser?.uid
+	if (!userId) return []
 
 	let query = firebase
 		.firestore()
 		.collection(collections.carts)
-		.where("userId", "==", userId);
-	const data = await query.get();
+		.where("userId", "==", userId)
+	const data = await query.get()
 	const cart = data.docs.map((doc) => ({
 		productId: doc.data().productId,
 		count: doc.data().count,
-	}));
+	}))
 
-	const productIds = cart.map((item) => item.productId);
-	const productQuery = firebase.firestore().collection(collections.products);
+	const productIds = cart.map((item) => item.productId)
+	const productQuery = firebase.firestore().collection(collections.products)
 
-	const productData = await productQuery.get();
+	const productData = await productQuery.get()
 	const products = productData.docs
 		.filter((doc) => productIds.includes(doc.id))
 		.map((doc) => ({
 			id: doc.id,
 			...doc.data(),
-		}));
+		}))
 
 	return products
 		.map((product) => {
-			const thisProduct = cart.find((item) => item.productId === product.id);
-			if (!thisProduct) return null;
+			const thisProduct = cart.find((item) => item.productId === product.id)
+			if (!thisProduct) return null
 			return {
 				id: product.id,
 				name: product.name,
 				price: product.price,
 				img: product.img,
 				count: thisProduct.count,
-			};
+			}
 		})
-		.filter(Boolean);
-};
+		.filter(Boolean)
+}
 
 export const removeProductInCart = async (productId) => {
-	const userId = firebase.auth().currentUser?.uid;
-	if (!userId) return;
+	const userId = firebase.auth().currentUser?.uid
+	if (!userId) return
 
 	let query = firebase
 		.firestore()
 		.collection(collections.carts)
-		.where("userId", "==", userId);
-	query = query.where("productId", "==", productId);
+		.where("userId", "==", userId)
+	query = query.where("productId", "==", productId)
 
-	const snapshot = await query.get();
-	snapshot.forEach((doc) => doc.ref.delete());
-};
+	const snapshot = await query.get()
+	snapshot.forEach((doc) => doc.ref.delete())
+}
 
 export const removeAllProductInCart = async () => {
-	const userId = firebase.auth().currentUser?.uid;
-	if (!userId) return;
+	const userId = firebase.auth().currentUser?.uid
+	if (!userId) return
 
 	let query = firebase
 		.firestore()
 		.collection(collections.carts)
-		.where("userId", "==", userId);
+		.where("userId", "==", userId)
 
-	const snapshot = await query.get();
-	snapshot.forEach((doc) => doc.ref.delete());
-};
+	const snapshot = await query.get()
+	snapshot.forEach((doc) => doc.ref.delete())
+}
