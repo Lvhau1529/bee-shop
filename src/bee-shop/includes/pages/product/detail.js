fae14286d87c1a42c77df5da63858c1d095e855b
jack.brightsoft <jack.brightsoft@gmail.com>
2021-07-25 21:37:11 +0700

    rating and comment service & UIs

--- src/bee-shop/includes/pages/product/detail.js
+++ src/bee-shop/includes/pages/product/detail.js
@@ -1,24 +1,46 @@
-import React, { useEffect, useState, useContext } from "react"
+import React, { useState, useContext } from "react"
 import { useParams } from "react-router-dom"
-import { Row, Col, Rate, InputNumber } from "antd"
+import format from "date-fns/format"
+import { Row, Col, Rate, InputNumber, Input, Button } from "antd"
 import Layout from "../../layouts/index"
 import Breadcrumb from "../../components/breadcrumb"
 import Slider from "./slider"
 
 import { ProductContext } from "../../../../contexts/ProductContext"
-import { getProductById } from "../../../../services/firebaseService"
+import { getProductById, getComments, createComments, getRatings, createRatings, editRating } from "../../../../services/firebaseService"
+import useUserAuth from "../../../../hooks/useUserAuth"
+
+const DATE_FORMAT = "dd-MM-yyyy HH:mm"
 
 function DeltailProduct() {
 	const context = useContext(ProductContext)
 	const { add } = context
 	const [data, setData] = useState({})
+	const [comments, setComments] = useState([])
+	const [text, setText] = useState("")
+	const [isDisabled, setIsDisabled] = useState(false)
+	const [ratings, setRatings] = useState({})
 	const { id } = useParams()
 	const [count, setCount] = useState(1)
+	const [isCommentMode, setIsCommentMode] = useState(false)
 
 	const getData = async () => {
-		const product = await getProductById(id)
-		setData(product)
+		try {
+			const [product, comments, ratings] = await Promise.all([
+				getProductById(id),
+				getComments(id),
+				getRatings(id)
+			])
+			setData(product)
+			setComments(comments)
+			setRatings(ratings)
+		} catch (err) {
+			console.log(err.response?.data || err.message)
+			alert(err.response?.data || err.message)
+		}
 	}
+
+	useUserAuth(getData, null)
 
 	const onChange = value => setCount(value)
 
@@ -31,9 +53,35 @@ function DeltailProduct() {
 		}
 	}
 
-	useEffect(() => {
-		getData()
-	}, [id])
+	const { numberOfRatings, average, yourRating } = ratings || {}
+
+	const onEditRating = async (value) => {
+		try {
+			if (!yourRating) {
+				await createRatings(id, value)
+			} else {
+				await editRating(yourRating.id, value)
+			}
+
+			const data = await getRatings(id)
+			setRatings(data)
+		} catch (err) {
+			alert(err.response?.data || err.message)
+		}
+	}
+
+	const onCreateComment = async () => {
+		setIsDisabled(true)
+		try {
+			await createComments(id, text)
+			const data = await getComments(id)
+			setText("")
+			setComments(data)
+		} catch (err) {
+			alert(err.response?.data || err.message)
+		}
+		setIsDisabled(false)
+	}
 
 	// utils
 	const shortenDescription = description => {
@@ -73,14 +121,21 @@ function DeltailProduct() {
 									</div>
 									<div className="detail__product-status d-flex justify-content-between align-items-center">
 										<div className="status__rate">
-											<Rate allowHalf defaultValue={5} />
+											<p><i>Average ratings ({numberOfRatings || 0} ratings)</i></p>
+											<Rate allowHalf value={average} disabled />
+											<p style={{ marginTop: 16 }}><i>Your ratings</i></p>
+											<Rate
+												allowHalf
+												value={yourRating?.value || 0}
+												min={1}
+												onChange={value => onEditRating(value)}
+											/>
 										</div>
 										<div className="status__available">Còn hàng</div>
 									</div>
 									<div className="detail__product-price d-flex align-items-center">
 										<div className="price__main">${data.price}</div>
 										{data.sale === 0 ? "" : <div className="price__sale">${data.sale}</div>}
-										{console.log(data.sale)}
 									</div>
 									<div className="detail__product-cart d-flex align-items-center">
 										<InputNumber
@@ -100,17 +155,39 @@ function DeltailProduct() {
 							<Col span={24}>
 								<div className="detail__tab">
 									<ul className="detail__tab-list d-flex">
-										<li className="list__item">
-											<a href>Miêu tả</a>
+										<li className="list__item cursor-pointer" onClick={() => setIsCommentMode(false)}>
+											<span style={{ fontSize: !isCommentMode ? "1.2em" : "1em" }}>Miêu tả</span>
 										</li>
-										<li className="list__item">
-											<a href>Nhận xét</a>
+										<li className="list__item cursor-pointer" onClick={() => setIsCommentMode(true)}>
+											<span style={{ fontSize: isCommentMode ? "1.2em" : "1em" }}>Nhận xét</span>
 										</li>
 									</ul>
 									<div className="detail__tab-content">
-										<p>
-											{data.description}
-										</p>
+										{isCommentMode
+											? <>
+												{
+													!!comments.length
+														? comments.map(comment => <div style={{ marginBottom: 16 }}>
+															<p style={{ margin: 0 }}><b>{comment.userEmail}</b></p>
+															<p style={{ margin: 0, fontSize: "0.8em" }}><i>{`${format(new Date(comment.time), DATE_FORMAT)}`}</i></p>
+															<span>{comment.value}</span>
+														</div>)
+														: <p>No comment.</p>
+												}
+												<div>
+													<Input
+														placeholder="Write something..."
+														value={text}
+														onChange={e => {
+															setText(e.target.value)
+														}}
+														onKeyUp={e => e.key === "Enter" && onCreateComment()}
+														disabled={isDisabled}
+													/>
+													<Button type="primary" style={{ marginTop: 8 }} onClick={onCreateComment}>Send comment</Button>
+												</div>
+											</>
+											: <p>{data.description}</p>}
 									</div>
 								</div>
 							</Col>
--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,14 +1,20 @@
-import firebase from "firebase";
+import firebase from "firebase"
 
 const config = {
-  apiKey: "AIzaSyDSL7zMB1aB0G8ECR1_7IbY3V0qhYTuCaE",
-  authDomain: "bee-shop-f73d0.firebaseapp.com",
-  projectId: "bee-shop-f73d0",
-  storageBucket: "bee-shop-f73d0.appspot.com",
-  messagingSenderId: "478089494339",
-  appId: "1:478089494339:web:fae17bbb6a2a63f80e39f6"
-};
+  // apiKey: "AIzaSyDSL7zMB1aB0G8ECR1_7IbY3V0qhYTuCaE",
+  // authDomain: "bee-shop-f73d0.firebaseapp.com",
+  // projectId: "bee-shop-f73d0",
+  // storageBucket: "bee-shop-f73d0.appspot.com",
+  // messagingSenderId: "478089494339",
+  // appId: "1:478089494339:web:fae17bbb6a2a63f80e39f6"
+  apiKey: "AIzaSyAovAoC4msXLP13u3-l6ZMLowxq39p_R58",
+  authDomain: "bee-shop-4ee5b.firebaseapp.com",
+  projectId: "bee-shop-4ee5b",
+  storageBucket: "bee-shop-4ee5b.appspot.com",
+  messagingSenderId: "456117310216",
+  appId: "1:456117310216:web:96d9d089a2d60460dad2bd"
+}
 
-const app = firebase.initializeApp(config);
+const app = firebase.initializeApp(config)
 
-export default app;
+export default app
